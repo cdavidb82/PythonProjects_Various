4411c980e4e17066e8c9f4cbb8593e06943b4e22
David Barnett <cdavidb82@outlook.com>
2022-04-16 12:08:53 -0400

    Updated code for input

--- PyWeather.py
+++ PyWeather.py
@@ -6,8 +6,10 @@ pp = pprint.PrettyPrinter(indent=2)
  Requires you to make an account to generate an API
  Make an account at https://openweathermap.org
 '''
+int_city = input('City: ')
+int_state = input('State: ')
 response = requests.get(
-    "https://api.openweathermap.org/data/2.5/weather?q=springfield,ohio&appid=6e86794d3d979323e578481f0d846126"
+    f"https://api.openweathermap.org/data/2.5/weather?q={int_city}, {int_state}&appid=6e86794d3d979323e578481f0d846126"
 )
 
 weather = response.json()
