f0726a2cc0ddaf70f98e5bf787c775afa0e7c5aa
David Barnett <cdavidb82@outlook.com>
2022-04-16 12:04:26 -0400

    New file commit

--- PyWeather.py
+++ PyWeather.py
@@ -2,6 +2,10 @@ import requests
 import pprint
 
 pp = pprint.PrettyPrinter(indent=2)
+'''
+ Requires you to make an account to generate an API
+ Make an account at https://openweathermap.org
+'''
 response = requests.get(
     "https://api.openweathermap.org/data/2.5/weather?q=springfield,ohio&appid=6e86794d3d979323e578481f0d846126"
 )
